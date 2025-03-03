a221b0d94a837535c23e508c9488d3a03bcfa82a
Govawi <76686935+Govawi@users.noreply.github.com>
2025-03-03 17:12:06 +0100

    Presets GM_1

---
+++ src/training/+presets/+network/PINN_GM_I.m
@@ -0,0 +1,22 @@
+function layers = PINN_GM_I()
+    layers = [ ...
+        featureInputLayer(3, "Name", "featureinput")
+        fullyConnectedLayer(20, "Name", "fc1")
+        geluLayer("Name", "act1")
+        fullyConnectedLayer(20, "Name", "fc2")
+        geluLayer("Name", "act2")
+        fullyConnectedLayer(20, "Name", "fc3")
+        geluLayer("Name", "act3")
+        fullyConnectedLayer(20, "Name", "fc4")
+        geluLayer("Name", "act4")
+        fullyConnectedLayer(20, "Name", "fc5")
+        geluLayer("Name", "act5")
+        fullyConnectedLayer(20, "Name", "fc6")
+        geluLayer("Name", "act6")
+        fullyConnectedLayer(20, "Name", "fc7")
+        geluLayer("Name", "act7")
+        fullyConnectedLayer(20, "Name", "fc8")
+        geluLayer("Name", "act8")
+        fullyConnectedLayer(1)
+    ];
+end
