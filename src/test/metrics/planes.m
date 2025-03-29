6532e1c4f7ad928aadf2e757793b3d8da0084ad2
Govawi <76686935+Govawi@users.noreply.github.com>
2025-03-29 17:10:25 +0100

    Planes metric

--- src/test/metrics/planes.m
+++ src/test/metrics/planes.m
@@ -1,16 +1,30 @@
-function val = planes(Y,T)
-% Evaluate custom metric.
+function val = planes(net, Trj, Acc)
 
-% Inputs:
-%          Y - Formatted dlarray of predictions
-%          T - Formatted dlarray of targets
-%
-% Outputs:
-%           val - Metric value
-%
-% Define the metric function here.
-diff = T - Y;
-p = vecnorm(diff) ./ vecnorm(T) * 100;
-val = sum(p) / size(p);
+    [PotPred, state] = forward(net, Trj);
+
+    % State data generated in the cart2sphLayer
+    RotationMatrix = state.Value(state.Layer == "cart2sphLayer" & state.Parameter == "RotationMatrix");
+    RotationMatrix = RotationMatrix{1};
+    Radius         = state.Value(state.Layer == "cart2sphLayer" & state.Parameter == "Radius");
+    Radius         = Radius{1};
+
+    % Preprocess Acceleration (rotate)
+    Acc = extractdata(Acc);
+    Acc = permute(Acc, [1, 3, 2]);
+    Acc = pagemtimes(RotationMatrix, Acc);
+    Acc = permute(Acc, [1, 3, 2]);
+    Acc = dlarray(Acc, 'CB');
+
+    % Preprocess Potential (proxy)
+    ScaleFactor                   = Radius;
+    ScaleFactor(ScaleFactor <= 1) = 1;
+    PotPred                       = PotPred ./ ScaleFactor;
+
+
+    AccPred = -dlgradient(sum(PotPred, 'all'), Trj, EnableHigherDerivatives = true);
+    
+    diff = Acc - AccPred;
+    p = vecnorm(diff) ./ vecnorm(Acc) * 100;
+    val = sum(p) / size(p,2);
 
 end
