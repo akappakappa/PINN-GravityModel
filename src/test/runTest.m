d368e468370b9ff6cec4e40bd28aaabe670ba1f0
akappakappa <andreavalde36@gmail.com>
2025-05-10 18:21:31 +0200

    testing formatting

--- src/test/runTest.m
+++ src/test/runTest.m
@@ -17,30 +17,25 @@ SurfaceTrj               = dlarray(readmatrix("src/preprocessing/datastore/metri
 SurfaceAcc               = dlarray(readmatrix("src/preprocessing/datastore/metrics/SurfaceAcc.csv"              ), 'BC');
 SurfacePot               = dlarray(readmatrix("src/preprocessing/datastore/metrics/SurfacePot.csv"              ), 'BC');
 
-% Load Network
-net = load("src/training/net.mat").net;
-
-% Planes Metric
-PlanesMetric         = dlfeval(@presets.planes        , net, PlanesTrj        , PlanesAcc        , PlanesPot        );
-fprintf("Planes metric         : %f\n", PlanesMetric        );
-
-% Generalization Metric
-GeneralizationMetric_0_1 = dlfeval(@presets.generalization, net, GeneralizationTrj_0_1, GeneralizationAcc_0_1, GeneralizationPot_0_1);
-fprintf("Generalization metric [0R:1R]: %f\n", GeneralizationMetric_0_1);
-
-GeneralizationMetric_1_10 = dlfeval(@presets.generalization, net, GeneralizationTrj_1_10, GeneralizationAcc_1_10, GeneralizationPot_1_10);
-fprintf("Generalization metric [1R:10R]: %f\n", GeneralizationMetric_1_10);
-
-GeneralizationMetric_10_100 = dlfeval(@presets.generalization, net, GeneralizationTrj_10_100, GeneralizationAcc_10_100, GeneralizationPot_10_100);
-fprintf("Generalization metric [10R:100R]: %f\n", GeneralizationMetric_10_100);
-
 GeneralizationTrj = cat(1, GeneralizationTrj_0_1, GeneralizationTrj_1_10, GeneralizationTrj_10_100);
 GeneralizationAcc = cat(1, GeneralizationAcc_0_1, GeneralizationAcc_1_10, GeneralizationAcc_10_100);
 GeneralizationPot = cat(1, GeneralizationPot_0_1, GeneralizationPot_1_10, GeneralizationPot_10_100);
 
-GeneralizationMetric = dlfeval(@presets.generalization, net, GeneralizationTrj, GeneralizationAcc, GeneralizationPot);
-fprintf("Generalization metric [0R:100R]: %f\n", GeneralizationMetric);
+% Load Network
+net = load("src/training/net.mat").net;
 
-% Surface Metric
-SurfaceMetric        = dlfeval(@presets.surface       , net, SurfaceTrj       , SurfaceAcc       , SurfacePot       );
-fprintf("Surface metric        : %f\n", SurfaceMetric       );
+% Compute metrics
+% ---------------------------------- | Preset function ----- | NN | Trajectory Data ------- | Acceleration Data ----- | Potential Data -------- |
+PlanesMetric                = dlfeval(@presets.planes        , net, PlanesTrj               , PlanesAcc               , PlanesPot               );
+GeneralizationMetric_0_1    = dlfeval(@presets.generalization, net, GeneralizationTrj_0_1   , GeneralizationAcc_0_1   , GeneralizationPot_0_1   );
+GeneralizationMetric_1_10   = dlfeval(@presets.generalization, net, GeneralizationTrj_1_10  , GeneralizationAcc_1_10  , GeneralizationPot_1_10  );
+GeneralizationMetric_10_100 = dlfeval(@presets.generalization, net, GeneralizationTrj_10_100, GeneralizationAcc_10_100, GeneralizationPot_10_100);
+GeneralizationMetric        = dlfeval(@presets.generalization, net, GeneralizationTrj       , GeneralizationAcc       , GeneralizationPot       );
+SurfaceMetric               = dlfeval(@presets.surface       , net, SurfaceTrj              , SurfaceAcc              , SurfacePot              );
+
+fprintf("Planes metric                   : %f\n", PlanesMetric               );
+fprintf("Generalization metric [0R:1R]   : %f\n", GeneralizationMetric_0_1   );
+fprintf("Generalization metric [1R:10R]  : %f\n", GeneralizationMetric_1_10  );
+fprintf("Generalization metric [10R:100R]: %f\n", GeneralizationMetric_10_100);
+fprintf("Generalization metric [0R:100R] : %f\n", GeneralizationMetric       );
+fprintf("Surface metric                  : %f\n", SurfaceMetric              );
