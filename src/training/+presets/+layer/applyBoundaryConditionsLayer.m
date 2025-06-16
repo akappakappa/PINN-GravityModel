00be12b716bfed83989c698fb960cdb786c64d9c
akappakappa <andreavalde36@gmail.com>
2025-06-16 18:31:21 +0200

    gm3 v2

--- src/training/+presets/+layer/applyBoundaryConditionsLayer.m
+++ src/training/+presets/+layer/applyBoundaryConditionsLayer.m
@@ -28,11 +28,11 @@ classdef applyBoundaryConditionsLayer < nnet.layer.Layer & nnet.layer.Accelerata
         function Potential = predict(layer, PotFused, PotLF, Radius)
             % Computes the potential at the given RADIUS, applying a smooth transition around 10R between the Fused Model and the Low-Fidelity Analytic Model.
 
-            % GELU
-            %weightBounds     = (1 + erf(layer.smoothness .* (Radius - layer.rref))) ./ 2;
-
             % Tanh
             %weightBounds     = (1 + tanh(layer.smoothness .* (Radius - layer.rref))) ./ 2;
+
+            % GELU
+            %weightBounds     = (1 + erf(layer.smoothness .* (Radius - layer.rref))) ./ 2;
 
             % Sigmoid
             %weightBounds     = 1 ./ (1 + exp(-layer.smoothness .* (Radius - layer.rref)));
