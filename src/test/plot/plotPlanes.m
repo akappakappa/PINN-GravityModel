ab68a1edb56bc4958b30fdf742590b045b726534
Govawi <76686935+Govawi@users.noreply.github.com>
2025-06-22 02:11:22 +0200

    plots

--- src/test/plot/plotPlanes.m
+++ src/test/plot/plotPlanes.m
@@ -2,10 +2,13 @@ function [] = plotPlanes(PlanesTRJ,PlanesMetric)
     % Planes Metric
     points = extractdata(PlanesTRJ)';
     errors = extractdata(PlanesMetric)';
-
+    figure;  % [left bottom width height]
+    t = tiledlayout(1, 3, 'TileSpacing', 'compact', 'Padding', 'compact');
+    
     % XY Plane
+    nexttile
+
     % Parameters
-    figure;
     z0 = 0;            % Choose the z-slice
     tol = 1e-6;        % Tolerance to filter points on the plane
 
@@ -33,21 +36,20 @@ function [] = plotPlanes(PlanesTRJ,PlanesMetric)
 
     % Step 2: Plot using surf with log10 data
     surf(X, Y, zeros(size(logErr)), logErr, 'EdgeColor', 'none');
-    view(2), axis equal tight;
+    view(2);
+    axis equal tight;
     colormap jet;
     grid off;
 
     % Step 3: Set color axis and colorbar ticks
     clim([-2 2])  % log10 scale from 10^-3 to 10^3
-    cb = colorbar;
-    log_ticks = -2:1:2;
-    cb.Ticks = log_ticks;
-    cb.TickLabels = arrayfun(@(x) sprintf('10^{%d}', x), log_ticks, 'UniformOutput', false);
-    ylabel(cb, 'Error Magnitude (log scale)', 'FontWeight', 'bold')
-
+   
     % YZ Plane
     % Parameters
-    figure;
+    %figure;
+    %subplot(1,3,2);
+    nexttile
+
     x0 = 0;
     tol = 1e-6;
 
@@ -75,20 +77,19 @@ function [] = plotPlanes(PlanesTRJ,PlanesMetric)
 
     % Step 2: Plot using surf with log10 data
     surf(Y, Zgrid, zeros(size(logErr)), logErr, 'EdgeColor', 'none');
-    view(2), axis equal tight;
+    view(2);
+    axis equal tight;
     colormap jet;
     grid off;
 
     % Step 3: Set color axis and colorbar ticks
     clim([-2 2])  % log10 scale from 10^-3 to 10^3
-    cb = colorbar;
-    log_ticks = -2:1:2;
-    cb.Ticks = log_ticks;
-    cb.TickLabels = arrayfun(@(x) sprintf('10^{%d}', x), log_ticks, 'UniformOutput', false);
-    ylabel(cb, 'Error Magnitude (log scale)', 'FontWeight', 'bold')
+    
 
     % XZ Plane
-    figure;
+    %figure;
+    %subplot(1,3,3);
+    nexttile
 
     % Parameters
     y0 = 0;
@@ -118,7 +119,8 @@ function [] = plotPlanes(PlanesTRJ,PlanesMetric)
 
     % Step 2: Plot using surf with log10 data
     surf(X, Zgrid, zeros(size(logErr)), logErr, 'EdgeColor', 'none');
-    view(2), axis equal tight;
+    view(2);
+    axis equal tight;
     colormap jet;
     grid off;
     % Step 3: Set color axis and colorbar ticks
--- src/test/plot/plotSurface.m
+++ src/test/plot/plotSurface.m
@@ -3,12 +3,13 @@ function [] = plotSurface(SurfaceTRJ,SurfaceMetric)
     points = extractdata(SurfaceTRJ)';
     errors = extractdata(SurfaceMetric)';
     logerrors = log10(errors);
+    figure;
+    t = tiledlayout(1, 3, 'TileSpacing', 'compact', 'Padding', 'compact');
 
     % Front view
-    figure;
+    nexttile;
     scatter3(points(:,1), points(:,2), points(:,3), 25, logerrors, "filled");
     clim([-2 2])  % log10 scale from 10^-3 to 10^3
-    cb = colorbar;
     view(2), axis equal padded;
     colormap jet;
     grid off;
@@ -27,17 +28,12 @@ function [] = plotSurface(SurfaceTRJ,SurfaceMetric)
     ylim([ylim_current(1) - expand_ratio*y_range, ylim_current(2) + expand_ratio*y_range])
     zlim([zlim_current(1) - expand_ratio*z_range, zlim_current(2) + expand_ratio*z_range]) 
 
-    log_ticks = -2:1:2;
-    cb.Ticks = log_ticks;
-    cb.TickLabels = arrayfun(@(x) sprintf('10^{%d}', x), log_ticks, 'UniformOutput', false);
-    ylabel(cb, 'Error Magnitude (log scale)', 'FontWeight', 'bold')
     view(0,0);
 
     % Side view
-    figure;
+    nexttile;
     scatter3(points(:,1), points(:,2), points(:,3), 25, logerrors, "filled");
     clim([-2 2])  % log10 scale from 10^-3 to 10^3
-    cb = colorbar;
     view(2), axis equal padded;
     colormap jet;
     grid off;
@@ -55,15 +51,10 @@ function [] = plotSurface(SurfaceTRJ,SurfaceMetric)
     xlim([xlim_current(1) - expand_ratio*x_range, xlim_current(2) + expand_ratio*x_range])
     ylim([ylim_current(1) - expand_ratio*y_range, ylim_current(2) + expand_ratio*y_range])
     zlim([zlim_current(1) - expand_ratio*z_range, zlim_current(2) + expand_ratio*z_range]) 
-
-    log_ticks = -2:1:2;
-    cb.Ticks = log_ticks;
-    cb.TickLabels = arrayfun(@(x) sprintf('10^{%d}', x), log_ticks, 'UniformOutput', false);
-    ylabel(cb, 'Error Magnitude (log scale)', 'FontWeight', 'bold')
     view(0,90);
 
     % Top view
-    figure;
+    nexttile;
     scatter3(points(:,1), points(:,2), points(:,3), 25, logerrors, "filled");
     clim([-2 2])  % log10 scale from 10^-3 to 10^3
     cb = colorbar;
