3bad29a6fe2a2e2c98f569ee1e901150f38d8294
akappakappa <andreavalde36@gmail.com>
2025-06-29 04:52:28 +0200

    scatter colors and regions

--- src/test/plot/plotGeneralization.m
+++ src/test/plot/plotGeneralization.m
@@ -2,16 +2,19 @@ function [] = plotGeneralization(nname, GeneralizationRadius, GeneralizationMetr
     % Generalization: mpeLoss vs. distance(R), convert mpeLoss in log scale
     figure;
     hold on;
-    semilogy(extractdata(GeneralizationRadiusPoly), extractdata(GeneralizationPoly)  , '.', 'Color', [127 127 127] ./ 256, 'DisplayName', 'Polyhedral');
-    semilogy(extractdata(GeneralizationRadius)    , extractdata(GeneralizationMetric), '.', 'Color', [142 27 29] ./ 256, 'DisplayName', 'Generalization');
+    xregion(0 , 1  , "FaceColor", [ 99  99  99] ./ 255, "EdgeColor", [0.5 0.5 0.5], "DisplayName", "Interior"     );
+    xregion(1 , 10 , "FaceColor", [149 149 149] ./ 255, "EdgeColor", [0.5 0.5 0.5], "DisplayName", "Exterior"     );
+    xregion(10, 100, "FaceColor", [199 199 199] ./ 255, "EdgeColor", [0.5 0.5 0.5], "DisplayName", "Extrapolation"); 
+    semilogy(extractdata(GeneralizationRadiusPoly), extractdata(GeneralizationPoly)  , '.', "MarkerSize", 6, "Color", [99 149 224] ./ 255, "DisplayName", "Polyhedral"    );
+    semilogy(extractdata(GeneralizationRadius)    , extractdata(GeneralizationMetric), '.', "MarkerSize", 6, "Color", [224 49 49] ./ 255, "DisplayName", "Generalization");
 
-    set(gca, 'YScale', 'log');
-    xlim([0, 20]);
+    set(gca, "YScale", "log");
+    xlim([0 20]);
     ylim([1e-4 1e3]);
     grid on;
-    xlabel('Distance (R)', 'FontSize', 12, 'FontWeight', 'bold', 'FontName', 'Palatino Linotype');
-    ylabel('Percent Error', 'FontSize', 12, 'FontWeight', 'bold', 'FontName', 'Palatino Linotype');
-    legend('show'); 
+    xlabel("Distance (R)" , "FontSize", 12, "FontWeight", "bold", "FontName", "Palatino Linotype");
+    ylabel("Percent Error", "FontSize", 12, "FontWeight", "bold", "FontName", "Palatino Linotype");
+    legend("show");
 
-    exportgraphics(gcf, "../../fig/" + nname + "/GNR_" + nname + ".png", 'Resolution', 300);
+    exportgraphics(gcf, "../../fig/" + nname + "/GNR_" + nname + ".png", "Resolution", 300);
 end
